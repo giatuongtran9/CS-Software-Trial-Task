db7a4e9b90d5286f353ec5b0e509275a41cbddf6
Gia Tuong Tran <trangiatuong999@gmail.com>
2022-02-12 13:46:56 -0500

    initial state

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+
+  // initial data
+  const [state, setState] = useState([
+    {
+      answer: 'Electricity is measured in units called watts',
+      clicked: false,
+      correct: false
+    },
+    {
+      answer: 'Electricity flows at the speed of light',
+      clicked: false,
+      correct: false
+    },
+    {
+      answer: 'Electricity is a primary energy source',
+      clicked: false,
+      correct: true
+    }
+  ])
+  
   return (
     <div className={styles.container}>
       
